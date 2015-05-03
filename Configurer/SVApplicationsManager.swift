68591ed5dbdf45f960897bfe7c15eabda504a0dd
Sebastien Villar <sebastien@sunrise.am>
2015-05-03 20:05:14 +0200

    SVApplicationManager > run configuration

--- Configurer/SVApplicationsManager.swift
+++ Configurer/SVApplicationsManager.swift
@@ -10,27 +10,59 @@ import Foundation
 import Cocoa
 
 class SVApplicationsManager: NSObject {
-  let operations: [String: kSVScreenArea]
+  let configuration: SVApplicationsConfiguration
   var applications = [SVApplication]()
   var observer: NSObject?
   
-  init(operations: Dictionary<String, kSVScreenArea>) {
-    self.operations = operations
+  init(configuration: SVApplicationsConfiguration) {
+    self.configuration = configuration
   }
   
   func run() {
     var screen = SVScreen(screen: NSScreen.mainScreen()!)
     
-    for (path, area) in self.operations {
+    // Launch window apps
+    for config in self.configuration.windowConfigurations {
+      switch config {
+      case .Window(let path, let area):
+        var application = SVApplication(path: path)
+        self.applications.append(application)
+        
+        application.observeLaunch() {
+          application.window?.rect = screen.rectForArea(area)
+        }
+        
+        application.launch()
+      default:
+        break
+      }
+    }
+    
+    // Lauch fullScreen apps
+    self.launchFullScreenAppAtIndex(0)
+  }
+  
+  private func launchFullScreenAppAtIndex(index: Int) {
+    var fullScreenConfigs = self.configuration.fullScreenConfigurations;
+    if (fullScreenConfigs.count <= index) {
+      return;
+    }
+        
+    var config = fullScreenConfigs[index]
+    
+    switch config {
+    case .FullScreen(let path):
       var application = SVApplication(path: path)
       self.applications.append(application)
       
-      application.observeLaunch() {
-        application.window?.rect = screen.rectForArea(area)
+      application.observeLaunch() { [weak self] in
+        application.window?.setFullScreen();
+        self?.launchFullScreenAppAtIndex(index + 1)
       }
       
       application.launch()
-      
+    default:
+      break
     }
   }
 }
