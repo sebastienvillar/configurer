bf23b46330d5299266bbdb0fb67ff1c062d8cc05
Sebastien Villar <sebastien@sunrise.am>
2015-05-03 17:08:00 +0200

    SVApplication > use path to create app and observe for activation of app

--- Configurer/SVApplication.swift
+++ Configurer/SVApplication.swift
@@ -7,13 +7,17 @@
 //
 
 import Foundation
+import Cocoa
 
 class SVApplication : NSObject {
-  private let element: AXUIElementRef
-  private let pid: pid_t
+  private let path: String
+  private var element: AXUIElementRef?
+  private var pid: pid_t?
+  private var launchObserver: NSObjectProtocol?
+  private var launchCallbacks = Array<Void -> Void>()
   
-  lazy var window: SVWindow? = {
-    var windowElement: AXUIElementRef? = self.element.getAttribute(kAXMainWindowAttribute)
+  lazy var window: SVWindow? = { [unowned self] in
+    var windowElement: AXUIElementRef? = self.element?.getAttribute(kAXMainWindowAttribute)
     if windowElement != nil {
       return SVWindow(element: windowElement!)
     }
@@ -21,10 +25,52 @@ class SVApplication : NSObject {
     return nil
   }()
   
-  init(pid: pid_t) {
-    self.pid = pid
-    self.element = AXUIElementCreateApplication(pid).takeUnretainedValue()
+  init(path: String) {
+    self.path = path
     
     super.init()
+    
+    self.startObservingLaunch()
+  }
+  
+  deinit {
+    self.stopObservingLaunch()
+  }
+  
+  func launch() {
+    NSWorkspace.sharedWorkspace().launchApplication(self.path)
+  }
+  
+  func observeLaunch(fn: Void -> Void) {
+    self.launchCallbacks.append(fn)
+  }
+  
+  private func startObservingLaunch() {
+    self.launchObserver = NSWorkspace.sharedWorkspace().notificationCenter.addObserverForName(NSWorkspaceDidActivateApplicationNotification, object: nil, queue: NSOperationQueue.mainQueue()) { [unowned self] notification in
+      if let dictionary: Dictionary = notification.userInfo {
+        var runningApplication = dictionary[NSWorkspaceApplicationKey] as? NSRunningApplication
+  
+        // If current application activated
+        var bundlePath = runningApplication?.bundleURL?.path
+        if bundlePath != nil && bundlePath!.hasSuffix(self.path) {
+          // Fill missing data
+          self.pid = runningApplication?.processIdentifier
+          self.element = AXUIElementCreateApplication(self.pid!).takeUnretainedValue()
+          
+          // Launch callbacks
+          for callback in self.launchCallbacks {
+            callback()
+          }
+          self.stopObservingLaunch()
+        }
+      }
+    }
+  }
+  
+  private func stopObservingLaunch() {
+    if self.launchObserver != nil {
+      NSWorkspace.sharedWorkspace().notificationCenter.removeObserver(self.launchObserver!)
+      self.launchObserver = nil
+    }
   }
 }
